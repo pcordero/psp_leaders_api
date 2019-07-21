c36bd729044fee49a8aa1bbc099cb9a464d6591c
Pablo Cordero <pablocordero@gmail.com>
2019-07-20 21:25:36 -0600

    Upgrade ruby version

--- Gemfile
+++ Gemfile
@@ -5,13 +5,15 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
+ruby '2.6.3'
 
+gem 'bootsnap', '>= 1.1.0', require: false
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 5.1.6'
+gem 'rails', '~> 5.2.3'
 # Use postgresql as the database for Active Record
 gem 'pg', '>= 0.18', '< 2.0'
 # Use Puma as the app server
-gem 'puma', '~> 3.7'
+gem 'puma'
 gem 'active_model_serializers'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 # gem 'jbuilder', '~> 2.5'
@@ -41,14 +43,14 @@ group :development do
   gem 'capistrano-asg'
   #gem 'capistrano-puma'
   gem 'capistrano3-puma', github: "seuros/capistrano-puma"
+  gem 'listen', '>= 3.0.5', '< 3.2'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
-#A 
+#A
 gem 'awesome_print'
 
 #R
 gem 'responders'
-
--- config/routes.rb
+++ config/routes.rb
@@ -17,23 +17,23 @@ Rails.application.routes.draw do
   # namespace :v1 do
   #   concerns :api_base
   # end
-  
+
   #get '/leaders/:id', to: 'leaders#show'
 
   # wget http://localhost:8080/v1/leaders/us-rep-ron-paul
   get '/v1/leaders/:id', to: 'leaders#show'
-  
+
   # wget http://localhost:3010/states/fl
   # wget http://localhost:3010/states/IN/leaders/state_senate
   # wget http://localhost:3010/states/FL/leaders/state_senate
-  
+
   #wget http://localhost:8080/v1/states/fl
   get '/v1/states/:id', to: 'leaders#states'
-  
+
   #wget http://localhost:8080/v1/states/fl/us_senate
   get '/v1/states/:state_id/us_senate', to: 'leaders#us_senate'
   get '/v1/states/:state_id/leaders/us_senate', to: 'leaders#us_senate'
-  
+
   #wget http://localhost:8080/v1/states/fl/us_house
   get '/v1/states/:state_id/us_house', to: 'leaders#us_house'
   get '/v1/states/:state_id/leaders/us_house', to: 'leaders#us_house'
@@ -41,11 +41,11 @@ Rails.application.routes.draw do
   #wget http://localhost:8080/v1/states/fl/state_senate
   get '/v1/states/:state_id/state_senate', to: 'leaders#state_senate'
   get '/v1/states/:state_id/leaders/state_senate', to: 'leaders#state_senate'
-  
+
   #/v1/states/ak/leaders/us_senate
   get '/v1/states/:state_id/us_senate', to: 'leaders#us_senate'
   get '/v1/states/:state_id/leaders/us_senate', to: 'leaders#us_senate'
-  
+
   #wget http://localhost:8080/v1/states/fl/state_house
   # http://localhost:9292/v1/states/ak/leaders/state_house
   get '/v1/states/:state_id/state_house', to: 'leaders#state_house'
@@ -53,17 +53,17 @@ Rails.application.routes.draw do
 
   #wget http://localhost:8080/v1/states/va/leaders
   get '/v1/states/:state_id/leaders', to: 'leaders#leaders'
-  
+
   # F, [2018-07-12T12:25:21.764888 #19004] FATAL -- : [a54266b8-063c-441d-bc04-1776a94aeb5c] ActionController::RoutingError (No route matches [GET] "/v1/states/oh/leaders/us_senate")
-  
+
   #get '/v1/states', to: ''
-  
+
   #/v1/states/fl/leaders
   ###get '/v1/states/:id/leaders', to: 'leaders#states'
   #namespace :v1 do
   #concerns :api_base do
   #resources :v1 do
-  resources :leaders 
+  resources :leaders
   resources :states do
     #get 'show'
     resources :leaders do
@@ -76,10 +76,9 @@ Rails.application.routes.draw do
     end
     #end
 end
-  
+
   # namespace :v1 do
   #   concerns :api_base
   # end
   #end
 end
-
